e66656bbee6b17391386840cef1fc3579e64a83d
Elvira Ramírez <elvirarp92@gmail.com>
2020-07-15 20:55:17 +0200

    Iterate over wanted key
    We have a list of keys that correspond to the data we want to dump into our CSV. So far, we can iterate over that list, check whether the key exists in our dictionary parsed from the JSON that the API gives us, and if it exists, it prints it out. This is an intermediate step for our final goal.

--- json2csv-from-api.py
+++ json2csv-from-api.py
@@ -3,12 +3,16 @@ import csv
 
 
 token = '648d70da-a1d2-3e97-b858-f61aa38c64f6'
-route = 'https://guiadigital.iaph.es/api/1.0/tesauro'
+id = 1
+route = 'https://guiadigital.iaph.es/api/1.0/bien/inmueble/' + str(id)
 headers = {'Authorization': 'Bearer ' + token}
+
+fields = ['id', 'codigo', 'municipio', 'provincia', 'denominacion', 'caracterizacion', 'proteccion_s', 'tipologiaList']
 
 r = requests.get(route, headers=headers, verify=False)
 
 json_response = r.json() #this is a dict
-json_data = json_response['response']['docs'] #this is a list!
 
-
+for field in fields:
+  if field in json_response:
+    print(json_response[field])
