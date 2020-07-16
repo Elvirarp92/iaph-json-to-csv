1b7f35f35b053a2af8d72789504d8b86796c0e88
Elvira Ramírez <elvirarp92@gmail.com>
2020-07-16 10:38:46 +0200

    CSV writing script now dumps non-iterable data to document
    We now have to find a way to write iterable data, but so far so good.

--- json2csv-from-api.py
+++ json2csv-from-api.py
@@ -7,12 +7,32 @@ id = 1
 route = 'https://guiadigital.iaph.es/api/1.0/bien/inmueble/' + str(id)
 headers = {'Authorization': 'Bearer ' + token}
 
-fields = ['id', 'codigo', 'municipio', 'provincia', 'denominacion', 'caracterizacion', 'proteccion_s', 'tipologiaList']
+unique_fields = ['id', 'codigo', 'municipio', 'provincia',
+                 'denominacion', 'caracterizacion', 'proteccion_s']
+tipologia_fields = ['crono_fin', 'crono_ini',
+                    'denom_acti', 'den_tipologia', 'periodos']
+
+csv_file_name = "File.csv"
 
 r = requests.get(route, headers=headers, verify=False)
 
-json_response = r.json() #this is a dict
+json_response = r.json()  # this is a dict
+tipologia_list = json_response['tipologiaList']['tipologia']
+data_row = {}
 
-for field in fields:
-  if field in json_response:
-    print(json_response[field])
+try:
+    csv_file_name = json_response["codigo"] + ".csv"
+    with open(csv_file_name, 'x') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=unique_fields)
+        writer.writeheader()
+        for field in unique_fields:
+            if field in json_response:
+                data_row[field] = json_response[field]
+            else:
+                data_row[field] = " "
+        writer.writerow(data_row)
+        data_row = {}
+        print(data_row)
+
+except IOError:
+    print("I/O error!")
