21c381b659e877bfd2e441a58e12959d7e8d4c80
Elvira Ramírez <elvirarp92@gmail.com>
2020-07-21 19:15:51 +0200

    Script now reads JSON from a file instead of an API response
    User wants to work with files in local, so we need to redefine where the script gets the JSON from.

--- json2csv.py
+++ json2csv.py
@@ -1,14 +1,8 @@
-import requests
+import json
 import csv
+import sys
 
-token = '648d70da-a1d2-3e97-b858-f61aa38c64f6'
-id = 23525  # Casa de Pilatos
-route = 'https://guiadigital.iaph.es/api/1.0/bien/inmueble/' + str(id)
-headers = {'Authorization': 'Bearer ' + token}
-
-r = requests.get(route, headers=headers, verify=False)
-
-json_response = r.json()  # this is a dict
+files = ["ficha-inmueble-23525.jsonld"]
 
 csv_headers = ['id', 'codigo', 'municipio', 'provincia',
                'denominacion', 'caracterizacion', 'proteccion_s',
@@ -16,32 +10,34 @@ csv_headers = ['id', 'codigo', 'municipio', 'provincia',
                'denom_acti', 'den_tipologia', 'periodos']
 
 try:
-    csv_file_name = json_response["codigo"] + ".csv"
+    csv_file_name = "IAPH_import.csv"
     with open(csv_file_name, 'x') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=csv_headers)
         writer.writeheader()
-        if isinstance(json_response["tipologiaList"]["tipologia"], dict):
-            writer.writerow({'id': json_response['id'], 'codigo': json_response['codigo'],
-                             'municipio': json_response['municipio'], 'provincia': json_response['provincia'],
-                             'denominacion': json_response['denominacion'], 'caracterizacion': json_response['caracterizacion'],
-                             'proteccion_s': json_response['proteccion_s'],
-                             'crono_fin': json_response['tipologiaList']['tipologia']['crono_fin'],
-                             'crono_ini': json_response['tipologiaList']['tipologia']['crono_ini'],
-                             'denom_acti': json_response['tipologiaList']['tipologia']['denom_acti'],
-                             'den_tipologia': json_response['tipologiaList']['tipologia']['den_tipologia'],
-                             'periodos': json_response['tipologiaList']['tipologia']['periodos']})
-        elif isinstance(json_response["tipologiaList"]["tipologia"], list):
-            for idx, elm in enumerate(json_response["tipologiaList"]["tipologia"]):
-                writer.writerow({'id': json_response['id'], 'codigo': json_response['codigo'],
-                                 'municipio': json_response['municipio'], 'provincia': json_response['provincia'],
-                                 'denominacion': json_response['denominacion'], 'caracterizacion': json_response['caracterizacion'],
-                                 'proteccion_s': json_response['proteccion_s'],
-                                 'crono_fin': json_response['tipologiaList']['tipologia'][idx]['crono_fin'],
-                                 'crono_ini': json_response['tipologiaList']['tipologia'][idx]['crono_ini'],
-                                 'denom_acti': json_response['tipologiaList']['tipologia'][idx]['denom_acti'],
-                                 'den_tipologia': json_response['tipologiaList']['tipologia'][idx]['den_tipologia'],
-                                 'periodos': json_response['tipologiaList']['tipologia'][idx]['periodos']})
-        print("Done!")
+        with open(files[0]) as json_file:
+            data = json.load(json_file)
+            if isinstance(data["tipologiaList"]["tipologia"], dict):
+                writer.writerow({'id': data['id'], 'codigo': data['codigo'],
+                                 'municipio': data['municipio'], 'provincia': data['provincia'],
+                                 'denominacion': data['denominacion'], 'caracterizacion': data['caracterizacion'],
+                                 'proteccion_s': data['proteccion_s'],
+                                 'crono_fin': data['tipologiaList']['tipologia']['crono_fin'],
+                                 'crono_ini': data['tipologiaList']['tipologia']['crono_ini'],
+                                 'denom_acti': data['tipologiaList']['tipologia']['denom_acti'],
+                                 'den_tipologia': data['tipologiaList']['tipologia']['den_tipologia'],
+                                 'periodos': data['tipologiaList']['tipologia']['periodos']})
+            elif isinstance(data["tipologiaList"]["tipologia"], list):
+                for idx, elm in enumerate(data["tipologiaList"]["tipologia"]):
+                    writer.writerow({'id': data['id'], 'codigo': data['codigo'],
+                                     'municipio': data['municipio'], 'provincia': data['provincia'],
+                                     'denominacion': data['denominacion'], 'caracterizacion': data['caracterizacion'],
+                                     'proteccion_s': data['proteccion_s'],
+                                     'crono_fin': data['tipologiaList']['tipologia'][idx]['crono_fin'],
+                                     'crono_ini': data['tipologiaList']['tipologia'][idx]['crono_ini'],
+                                     'denom_acti': data['tipologiaList']['tipologia'][idx]['denom_acti'],
+                                     'den_tipologia': data['tipologiaList']['tipologia'][idx]['den_tipologia'],
+                                     'periodos': data['tipologiaList']['tipologia'][idx]['periodos']})
+            print("Done!")
 
 except IOError:
     print("I/O error!")
