18075b014de57b68ba81262aa93eebaf7f6a0010
Ismael Olea <ismael@olea.org>
2020-07-25 14:11:02 +0200

    funcionan las tipologiaList

--- json2csv-olea.py
+++ json2csv-olea.py
@@ -7,91 +7,98 @@ files = sys.argv[1:]
 
 
 with open('IAPH_import.csv', mode='w') as IAPH_import_file:
-    # fieldnames = ['id', "codigo", "denominacion", "tipo",
-    #     "municipio", "provincia", "caracterizacion",
-    #     "crono_ini", "crono_fin", "periodos", "den_tipologia","denom_acti", "den_estilo"
-    #   ]
-    csv_headers = ['id', 'codigo', 'denominacion', 
+    csv_headers = ['id', 'codigo', 'denominacion', 'otros_nombres', 'tipo',
           'municipio', 'provincia', 'caracterizacion', 
           'crono_ini', 'crono_fin', 
-          'denom_acti', 'den_tipologia', 'periodos',
+          'denom_acti', 'den_tipologia', 'periodos', 'den_estilo',
           'proteccion_s']
     writer = csv.DictWriter(IAPH_import_file, fieldnames=csv_headers)
     writer.writeheader()
 
     while files:
-      print ("ahora vamos con", files[0], "\n")
+      print ("\n\nahora vamos con", files[0], "\n")
       with open( files[0] ) as f:
         data = json.load(f)
-        print (data["id"])
+        registro_csv = {}
+        registro_csv["id"] = data["id"]
+        registro_csv["codigo"] = data["codigo"]
+        registro_csv["denominacion"] = data["denominacion"]
+        if "tipo_contenido" in data:
+          registro_csv["tipo"] = data["tipo_contenido"]
+        else :
+          registro_csv["tipo"] = ""
+        if "municipio" in data:
+          registro_csv["municipio"] = data["municipio"]
+        else :
+          registro_csv["municipio"] = ""
+        if "provincia" in data:
+          registro_csv["provincia"] = data["provincia"]
+        else:
+          registro_csv["provincia"] = ""
+        if "caracterizacion" in data:
+          registro_csv["caracterizacion"] = data["caracterizacion"]
+        else:
+          registro_csv["caracterizacion"] = ""
+        registro_csv["proteccion_s"] = data['proteccion_s']
 
-        # registro_csv = {}
-        # registro_csv["id"] = data["id"]
-        # registro_csv["codigo"] = data["codigo"]
-        # registro_csv["denominacion"] = data["denominacion"]
-        # if "tipo_contenido" in data:
-        #   registro_csv["tipo"] = data["tipo_contenido"]
-        # else :
-        #   registro_csv["tipo"] = ""
-        # if "municipio" in data:
-        #   registro_csv["municipio"] = data["municipio"]
-        # else :
-        #   registro_csv["municipio"] = ""
-        # if "provincia" in data:
-        #   registro_csv["provincia"] = data["provincia"]
-        # else:
-        #   registro_csv["provincia"] = ""
-        # if "caracterizacion" in data:
-        #   registro_csv["caracterizacion"] = data["caracterizacion"]
-        # else:
-        #   registro_csv["caracterizacion"] = ""
+        try:
+          if isinstance(data['denominacionList'], dict):
+            registro_csv['otros_nombres'] = data['denominacionList']['denominacion']
+          elif  isinstance(data['denominacionList'], list):
+            for idx, elm in enumerate(data["denominacionList"]):
+              registro_csv['otros_nombres'] = data['denominacionList'][idx]['denominacion']
+        except:
+          registro_csv['otros_nombres'] = ""
 
+        print("Otros nombres: ",registro_csv['otros_nombres'])  
 
-        # writer.writerow({
-        #   'id': registro_csv["id"],
-        #   "codigo": registro_csv["codigo"],
-        #   "denominacion": registro_csv["denominacion"],
-        #   "tipo": registro_csv["tipo"],
-        #   "municipio": registro_csv["municipio"],
-        #   "provincia": registro_csv["provincia"],
-        #   "caracterizacion": registro_csv["caracterizacion"],
-        #   # "crono_ini": data["tipologiaList"]["tipologia"]["crono_ini"],
-        #   # "crono_fin": data["tipologiaList"]["tipologia"]["crono_fin"],
-        #   # "periodos": data["tipologiaList"]["tipologia"]["periodos"],
-        #   # "den_tipologia": data["tipologiaList"]["tipologia"]["den_tipologia"],
-        #   # "denom_acti": data["tipologiaList"]["tipologia"]["denom_acti"],
-        #   # "den_estilo": data["tipologiaList"]["tipologia"]["den_estilo"]
-        #   })
-
+        tipologiaList = []        
         if isinstance(data["tipologiaList"]["tipologia"], dict):
-          writer.writerow({
-            'id': data['id'], 
-            'codigo': data['codigo'],
-            'municipio': data['municipio'], 
-            'provincia': data['provincia'],
-            'denominacion': data['denominacion'],
-            'caracterizacion': data['caracterizacion'],
-            'proteccion_s': data['proteccion_s'],
-            'crono_fin': data['tipologiaList']['tipologia']['crono_fin'],
-            'crono_ini': data['tipologiaList']['tipologia']['crono_ini'],
-            'denom_acti': data['tipologiaList']['tipologia']['denom_acti'],
-            'den_tipologia': data['tipologiaList']['tipologia']['den_tipologia'],
-            'periodos': data['tipologiaList']['tipologia']['periodos']})
+          tipologiaList.append([ 
+              data['tipologiaList']['tipologia']['crono_ini'],
+              data['tipologiaList']['tipologia']['crono_fin'], 
+              data['tipologiaList']['tipologia']['denom_acti'],
+              data['tipologiaList']['tipologia']['den_tipologia'],
+              data['tipologiaList']['tipologia']['periodos'],
+              data['tipologiaList']['tipologia']['den_estilo']
+          ]) 
         elif isinstance(data["tipologiaList"]["tipologia"], list):
           for idx, elm in enumerate(data["tipologiaList"]["tipologia"]):
-              writer.writerow({
-                'id': data['id'], 
-                'codigo': data['codigo'],
-                'municipio': data['municipio'], 
-                'provincia': data['provincia'],
-                'denominacion': data['denominacion'], 
-                'caracterizacion': data['caracterizacion'],
-                'proteccion_s': data['proteccion_s'],
-                'crono_fin': data['tipologiaList']['tipologia'][idx]['crono_fin'],
-                'crono_ini': data['tipologiaList']['tipologia'][idx]['crono_ini'],
-                'denom_acti': data['tipologiaList']['tipologia'][idx]['denom_acti'],
-                'den_tipologia': data['tipologiaList']['tipologia'][idx]['den_tipologia'],
-                'periodos': data['tipologiaList']['tipologia'][idx]['periodos']})
-                                                 
+            tipologiaList.append([ 
+              data['tipologiaList']['tipologia'][idx]['crono_ini'],
+              data['tipologiaList']['tipologia'][idx]['crono_fin'], 
+              data['tipologiaList']['tipologia'][idx]['denom_acti'],
+              data['tipologiaList']['tipologia'][idx]['den_tipologia'],
+              data['tipologiaList']['tipologia'][idx]['periodos'],
+              data['tipologiaList']['tipologia'][idx]['den_estilo']
+            ])
+
+        writer.writerow({
+          'id': registro_csv["id"],
+          "codigo": registro_csv["codigo"],
+          "denominacion": registro_csv["denominacion"],
+          "otros_nombres": registro_csv['otros_nombres'],
+          "tipo": registro_csv["tipo"],
+          "municipio": registro_csv["municipio"],
+          "provincia": registro_csv["provincia"],
+          "caracterizacion": registro_csv["caracterizacion"],
+          "crono_ini": tipologiaList[0][0],
+          "crono_fin": tipologiaList[0][1],
+          "denom_acti": tipologiaList[0][2],
+          "den_tipologia": tipologiaList[0][3],
+          "periodos": tipologiaList[0][4],
+          "den_estilo": tipologiaList[0][5],
+          "proteccion_s": registro_csv["proteccion_s"]
+          })
+
+        for value in tipologiaList: 
+          writer.writerow({
+            "crono_ini": value[0],
+            "crono_fin": value[1],
+            "denom_acti": value[2],
+            "den_tipologia": value[3],
+            "den_estilo": value[4]
+          })
+
         f.close()
-        print(files.pop(0))
+        print(files.pop(0), "procesado.")
